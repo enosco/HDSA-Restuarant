316f502f198beb5e14d32c1efe076b0d5184c419
enosco <scottrweno@gmail.com>
2024-11-24 19:49:52 -0500

    specified dataflow for Driver methods + completed impl. for program startup

--- Driver.java
+++ Driver.java
@@ -1,76 +1,220 @@
+import java.io.*;
 public class Driver
 {
-    public static void main(String[] args)
+    static BufferedReader stdin = new BufferedReader (new InputStreamReader(System.in));
+    
+    public static void main(String[] args) throws IOException
     {
-	int option = 0;
-	switch (option)
-	{
-	    case 0: // exit
-	    case 1: // party enters
-	    case 2: // serve waiting party
-	    case 3: // party leaves
-	    case 4: // add table
-	    case 5: // remove table
-	    case 6: // display available tables
-	    case 7: // display waiting parties
-	    case 8: // display occupied tables
+	List<Section> sections = new List<Section>();
+	Queue<Party> waiting = new Queue<Party>();
+	
+	initialize(sections);
+
+	System.out.print("Make your menu selection now: ");
+	int option = Integer.parseInt(stdin.readLine().trim());
+	System.out.println(option);
+	
+	while (option != 0) {
+	    switch (option) {
+		case 0: break;// exit		    
+		case 1: // party enters
+		    welcomeParty(waitingQueue);
+		    break;
+		case 2: // serve waiting party
+		    seatParty(sections, waiting);
+		    break;
+		case 3: // party leaves
+		    removeParty(sections, waiting);
+		    break;
+		case 4: // add table
+		    addTable(sections);
+		    break;
+		case 5: // remove table
+		    removeTable(sections);
+		    break;
+		case 6: // display available tables
+		    displayAvailableTables(sections);
+		    break;
+		case 7: // display waiting parties
+		    displayWaiting(waiting);
+		    break;
+		case 8: // display occupied tables
+		    displayServing(sections);
+		    break;
+	    }
+	    
+	    System.out.print("You know the options. Make your menu selection now: ");
+	    option = Integer.parseInt(stdin.readLine().trim());
+	    System.out.println(option);
 	}
+	
+	System.out.println("We are closing the restaurant...Good Bye!");
+    }
+       
+    public static void initilize(List<Section> sections)
+    {
+	// Sections are hard coded for now. Later, prompt the user
+	// for the number of sections they want.
+	sections.add(new Section(0, "pet-friendly"));
+	sections.add(new Section(1, "non-pet-friendly"));
+	
+	System.out.println("Enter your restaurant configuration:");
+	// For each section
+	int size = section.size();
+	for (int i = 0; i < size; i++) {	    
+	    // Prompt user for number of tables
+	    Section sect = sections.get(i);
+	    
+	    System.out.print(">>How many tables does your " + sect.getName() + " section have? ");
+	    int numTables = Integer.parseInt(stdin.readLine().trim());
+	    System.out.println(numTables);
+	    
+	    // For [number of tables] loops
+	    for (int i = 0; i < numTables; i++) {
+		
+                // Prompt user for tableName until unique name is given	       
+		System.out.print(">>Enter table name:");
+		String tableName = stdin.readLine().trim();
+		System.out.println(tableName);
+
+		
+		// If section.hasTable(tableName) == true → tableName is not unique, prompt again	      
+                // Repeat until unique name is given
+		while (sect.hasTable(tableName)) {
+
+		    System.out.println("This table already exists! Please enter another table name.");
+		    
+		    System.out.print(">>Enter table name:");
+		    tableName = stdin.readLine().trim();
+		    System.out.println(tableName);
+		}
+
+		// Prompt for number of seats
+		System.out.print(">>Enter number of seats:");
+		int numSeats = Integer.parseInt(stdin.readLine().trim());
+		System.out.println(numTables);
+
+		// Create newTable
+		// section.addTable(newTable)
+		// newTable is added to availableTables
+		sect.addTable(new Table(tableName, numSeats));
+      		
+	    }
+	    
+	}
+
+	printMen();
+	// Print menu and begin
+
+
     }
 
-    public static void welcomeParty()
+    public static void printMenu()
     {
-	// prompt for party name
-	// if already exists -> prompt until unique name
-	// prompt for party size, pets/no pets
+	System.out.println("Select from the following menu:\n"
+			   + "\t0.  Close the restaurant.\n"
+			   + "\t1.   Customer party enters the restaurant.\n"
+			   + "\t2.   Customer party is seated and served.\n"
+			   + "\t3.   Customer party leaves the restaurant.\n"
+			   + "\t4.   Add a table.\n"
+			   + "\t5.   Remove a table.\n"
+			   + "\t6.   Display available tables.\n"
+			   + "\t7.   Display info about waiting customer parties.\n"
+			   + "\t8.   Display info about customer parties being served.\n\n");  
+    }
 
-	// create new party and add to waiting collection
-
-	
+    
+    public static void welcomeParty()
+    {	
+	// Prompt for partyName until unique name is given
+        	// if waitingQueue contains party with partyName OR section.hasParty(partyName) == true → partyName is not unique, prompt again
+	// Prompt for group size
+	// Prompt for hasPets
+	// Create newParty
+	// Enqueue newParty onto waitingQueue
     }
 
     public static void seatParty()
     {
-	// attempt to seat party at front of queue
-	// if no tables available: attempt to seat next party,
-	//    continue until party is seated or noone could be seated
+	// Dequeue nextParty from the front of waitingQueue
+	// Store nextParty’s name to keep track of original order of the queue
+	// Loop until nextParty is seated OR no party could be seated (nextParty == original front of queue)
+        	// Get the section that nextParty prefers
+	        // preferredSection.add(nextParty)
+                 	// Search through availableTables to find a table with the least number of excess seats.
+                        	// If a Table is found, create a new OccupiedTable from nextParty and Table and add it to occupiedTables
+                                	// .add(nextParty) returns true
+                         	// if .add(nextParty) returns false → enqueue nextParty, dequeue new nextParty, repeat
+                                 	// Display failure 
+	// If a party was successfully seated → rotate through queue items until original FIFO order is restored
+        	// dequeue and enqueue from waitingQueue until peek returns original front of queue -> Display success
+	// If no parties could be seated -->  Display failure
     }
 
     public static void removeParty()
     {
-	// prompt for party name
-	// if it matches a party currently being served, free the table occupied by the party
+	// Prompt for partyName of leaving party
+	// For each section until a match is found
+        	// section.remove(partyName)
+        	// Search occupiedTables for matching 
+                	// if a match is found:
+                        	// remove and secure matching OccupiedTable from occupiedTables
+                         	// Create newTable from name and seats of OccupiedTable
+                        	// add newTable to availableTables 
+                                	// Table is now freed
+                         	// Return matching OccupiedTable
+                                   	// Display successful remove
+	// If a match is not found within any of the sections
+         	// search through waitingList to verify that specified party is not still waiting
+         	// Display location of match in waitingList
+	// Else → match not found, display failure
     }
 
     public static void addTable()
     {
-	// prompt for table name
-	// if already exists -> halt
-	
-	// prompt for section
+	// Prompt user for section to add to
+	// Prompt user for tableName until unique name is given
+         	// If section.hasTable(tableName) == true → tableName is not unique, prompt again
+                	// Repeat until unique name is given
+	// Prompt for number of seats
+	// Create newTable
+	// section.addTable(newTable)
+        	// newTable is added to availableTables
     }
 
     public static void removeTable()
     {
-	// prompt for section
-	// prompt for table name
-	// if it does not exist -> halt
-	// if occupied -> halt
-	// else, remove table 
+	// Prompt user for section to remove from
+	// Prompt user for tableName
+	// section.removeTable()
+        	// If matching tableName is found in availableTables
+                	// Remove, secure, and return found Table
+                  	// Display successful remove
+            	// If matching tableName is found in occupiedTables
+                	// Throw RemoveOccupiedTableException
+                 	// Driver catches exception, informs user that table is occupied
+          	// If matching tableName could not be found
+                 	// Throw NoSuchElementException
+                   	// Driver catches exception, informs user that table could not be found
+
     }
 
     public static void displayAvailableTables()
     {
-	// if available tables is not empty ->
-
+	// For each section
+        	// section.getAvailableTableInfo()
+                  	// Display name of section along with size and contents of availableTables (.size() & .toString() )
     }
 
     public static void displayWaiting()
     {
-	// if waitlist is not empty -> display
+	// Display contents of waitingList (.toString() )
     }
 
     public static void displayServing()
     {
-	// if seated list is not empty -> display
+	// For each section
+        	// section.getServingInfo()
+                	// Display name of section along with contents of occupiedTables (.size() & .toString() )
     }
 }
